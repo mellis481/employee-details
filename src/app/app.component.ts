d70dc9a69b27f684ecdf22a3d671006ca63bfddc
Michael Ellis <mellis@anexinet.com>
2020-04-22 11:54:24 -0400

    Added single-spa props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import {
+  SingleSpaProps,
+  singleSpaPropsSubject,
+} from 'src/single-spa/single-spa-props';
 
 export interface Employee {
   id: number;
@@ -14,12 +19,18 @@ export interface Employee {
   selector: 'employee-details-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   employee: Employee | undefined;
+  singleSpaProps: SingleSpaProps = null;
+  subscription: Subscription = null;
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.subscription = singleSpaPropsSubject.subscribe(
+      (props) => (this.singleSpaProps = props)
+    );
+
     // TODO: resolve the ID from the route rather than window.location
     // const id = this.route.snapshot.paramMap.get('id');
     const id = window.location.pathname.substr(
@@ -30,5 +41,9 @@ export class AppComponent implements OnInit {
         response.json().then((data) => (this.employee = data));
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
